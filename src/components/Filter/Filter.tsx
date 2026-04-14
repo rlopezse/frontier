f8e8298c451f383d461680754a768836111fafd4
rlopezse <rza.lopez.s@gmail.com>
2026-04-13 22:12:44 -0400

    added: algo de css para que se vea mejor

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -6,10 +6,10 @@ const sizes: Size[] = ['XS', 'S', 'M', 'ML', 'L', 'XL', 'XXL']
 const Filter = () => {
   return (
     <div className={s.filter}>
-      <h3>Sizes:</h3>
-      <div className="filter-sizes">
+      <h3 className={s.filter_title}>Talla</h3>
+      <div className={s.filter_buttons}>
         {sizes.map((size) => (
-          <button key={size} className="filter_size_button">
+          <button key={size} className={s.filter_button}>
             {size}
           </button>
         ))}
